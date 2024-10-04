c10b84ad5f99b534ed4895a2494adc153b5cb161
kerbegneto <kerbegneto@gmail.com>
2024-10-04 01:14:13 -0300

    alteração da extensão do arquivo de massa de dados de Json para js

--- __tests__/unit/calculadora.espec.js
+++ __tests__/unit/calculadora.espec.js
@@ -3,7 +3,7 @@
 const calculadora = require("../../src/calculadora");
 
 // Apontamento para o arquivo de massa de teste
-const arquivo = require("../../vendorscsv/massaDivisao.json");
+const arquivoJson = require("../../vendors/csv/massaDivisao");
 
 // Funções de testes de Unidade
 test("Somar 5 + 7", () => {
@@ -95,5 +95,22 @@ test.each(massaDivisao)("Dividir %f / %f", (num1, num2, resultadoEsperado) =>{
     const resultadoAtual = dividirDoisNumeros(num1, num2);
     // Validar 
 
+    expect(resultadoAtual).toBe(resultadoEsperado);
+})
+
+test.each(arquivoJson.array.map(elemento => [
+    elemento.num1,
+    elemento.num2,
+    elemento.resultadoEsperado
+]))
+
+("DDT: Dividir %f / %f", (num1, num2, resultadoEsperado) => {
+    // configura
+
+    //Executa
+    const dividirDoisNumeros = calculadora.dividirDoisNumeros;
+    const resultadoAtual =dividirDoisNumeros(num1, num2);
+
+    //Valida
     expect(resultadoAtual).toBe(resultadoEsperado);
 })
